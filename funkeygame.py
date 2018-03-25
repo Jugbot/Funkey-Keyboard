6f5df335492f5adc747b6b4b6c76fc46f6dd84ac
Jugbot <lucasp465@gmail.com>
2018-03-25 14:03:15 -0400

    fixed N

--- funkeygame.py
+++ funkeygame.py
@@ -40,7 +40,7 @@ LED_COUNT = 100      # Number of LED pixels.
 LED_PIN = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10       # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 128  # Set to 0 for darkest and 255 for brightest
 # True to invert the signal (when using NPN transistor level shift)
 LED_INVERT = False
 
@@ -50,12 +50,12 @@ RIGHT = 2
 
 START_DELAY = 3.0 #three seconds before song starts
 
+# Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+# Intialize the library (must be called once before other functions).
+strip.begin()
+
 def main():
-    
-    # Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-    # Intialize the library (must be called once before other functions).
-    strip.begin()
     midifile = displaymenu(strip)        
     #read midi file and convert from relative time to absolute
     pattern = midi.read_midifile(midifile)
@@ -105,7 +105,7 @@ class Note:
         r,g,b = [int(256*i) for i in colorsys.hls_to_rgb(rcol,0.5,1.0)] 
         self.color = Color(r, g, b)
         '''
-        self.color = Color(255,255,255)
+        self.color = Color(0,255,0)
         #used by rendering
         self.laststart = 7
         self.laststop = 7
